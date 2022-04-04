567d66fd26cb7fcb7e5007648ab212d3c41a8ad2
baroka <birotori@gmail.com>
2022-04-04 19:23:53 +0100

    Add feature menu js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector("#id-menu-bar");
+const logoText = document.querySelector(".logo-text");
+const menuBar = document.querySelector(".menu-bar");
+
 
 navToggle.addEventListener("click", () => {
   primaryNav.classList.toggle("active");
+  logoText.classList.toggle("active");
+  menuBar.classList.toggle("active");
 });
